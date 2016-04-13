c0e7cc0e9fe844ce9adaa48b00005be2fbe8eb35
zhangjinglin <zhangjinglin@jibird.com>
2016-04-13 17:47:21 +0800

    test

--- src/magic.js
+++ src/magic.js
@@ -49,10 +49,57 @@ magic.fn = magic.prototype = {
            fn.call(this[i],i);
        }
     },
-
+    css:function(object,style){
+        if(typeof(object) == "string"){
+            this.each(function(i){
+                var name = object.toLowerCase();
+                style = style.toLowerCase();
+                this.style[name] = style;
+            })
+        }else if(typeof(object)=="object"&&!style){
+            var _this = this;
+            magic.each(object,function(i,n){
+                var name = i.toLowerCase();
+                style = n.toLowerCase();
+                _this.each(function(){
+                    this.style[name] = style;
+                })
+            })
+        }
+        return this;
+    },
+    html:function(html){},
+    text:function(text){},
+    parent:function(){},
+    parents:function(){},
+    children:function(){},
+    next:function(){},
+    prev:function(){},
+    val:function(){},
+    append:function(){},
+    prepend:function(){},
+    appendTo:function(){},
+    prependTo:function(){},
+    before:function(){},
+    after:function(){},
+    insertBefore:function(){},
+    insertAfter:function(){},
+    remove:function(){},
+    siblings:function(){},
+    eq:function(){},
+    hasClass:function(){},
+    animate:function(){},
+    show:function(){},
+    hide:function(){},
+    offset:function(){},
+    scrollTop:function(){},
+    attr:function(){},
+    removeAttr:function(){},
+    data:function(){},
+    removeData:function(){}
 }
 magic.fn.selector.prototype = magic.fn;
-//
+//此方法伪装对象类型
 magic.makeArray = function(selector){
         var _this = this;
         var arr = [];
@@ -77,13 +124,25 @@ magic.makeArray = function(selector){
         }
        return _this;
 }
+//此方法遍历数组对象
+magic.each = function(object,fn){
+     if(object){
+         for(var i in object){
+             if(object.hasOwnProperty(i)){
+                 fn.call(object[i],i,object[i]);
+             }
+         }
+     }
+}
 //
 
 //
 function each(object,fn){
-    for(var i in object){
-        if(object.hasOwnProperty(i)){
-            fn.call(object[i],i);
+    if(object) {
+        for (var i in object) {
+            if (object.hasOwnProperty(i)) {
+                fn.call(object[i], i);
+            }
         }
     }
 }
